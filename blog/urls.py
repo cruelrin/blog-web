c00de0a71f44f83dd14253e4e90ce635d8626860
Sheveleva Karina <karinasheveleva1364@gmail.com>
2024-02-15 23:23:06 +0600

    2 коммит

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
